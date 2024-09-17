70ff9f9f328f2be47dd217cd8098fee6a503ecab
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2024-09-17 10:10:22 +0330

    feat: update cors

--- src/app/api/set-cookie/route.ts
+++ src/app/api/set-cookie/route.ts
@@ -25,6 +25,7 @@ export function POST(req: Request){
     headers: {
       'content-type': 'application/json',
       'Access-Control-Allow-Origin': 'https://bb.vuecasts.ir',
+      'Access-Control-Allow-Credentials': 'true',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     }
